90e24f0544f311b1ffdda1d21d657c443a05232b
kpickreign <pickreig@bc.edu>
2019-02-22 19:12:25 -0500

    6.4 added switch case in func

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -18,12 +18,39 @@ class ViewController: UIViewController {
     var formulaArray = ["miles to kilometers", "kilometers to miles", "feet to meters", "yards to meters", "meters to feet", "meters to yards"]
     var fromUnits = ""
     var toUnits = ""
+    var conversionString = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         formulaPicker.delegate = self
         formulaPicker.dataSource = self
     }
+    
+    func calculateConversion(){
+        var outPutValue = 0.0
+        if let inputValue = Double(userInput.text!){
+            switch conversionString {
+            case "miles to kilometers":
+                outPutValue = inputValue / 0.62137
+            case "kilometers to miles":
+                outPutValue = inputValue * 0.62137
+            case "feet to meters":
+                outPutValue = inputValue / 3.2808
+            case "yards to meters":
+                outPutValue = inputValue / 1.0936
+            case "meters to feet":
+                outPutValue = inputValue * 3.2808
+            case "meters to yards":
+                outPutValue = inputValue * 1.0936
+            default:
+                print("show alert-for some reason we did not have a conversion string")
+            }
+            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outPutValue) \(toUnits)"
+        } else {
+            print("Show alert here to say the value entered was not a number")
+        }
+    }
+
     
     @IBAction func convertButtonPressed(_ sender: UIButton) {
     }
@@ -45,11 +72,12 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        conversionString = formulaArray[row]
         let unitsArray = formulaArray[row].components(separatedBy: " to ")
         fromUnits = unitsArray[0]
         toUnits = unitsArray[1]
         fromUnitsLabel.text = fromUnits
-        resultsLabel.text = toUnits
+        calculateConversion()
     }
     
 }
